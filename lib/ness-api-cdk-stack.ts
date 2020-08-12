e562fa46959a14990fed947fbfc024d3a6b276eb
uji <ujiprog@gmail.com>
2020-08-12 23:30:54 +0900

    feat: s3 turorial

--- lib/ness-api-cdk-stack.ts
+++ lib/ness-api-cdk-stack.ts
@@ -1,9 +1,20 @@
 import * as cdk from '@aws-cdk/core';
+import * as s3 from '@aws-cdk/aws-s3'
+// import * as lambda from '@aws-cdk/aws-lambda'
+// import * as path from 'path'
 
 export class NessApiCdkStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    new s3.Bucket(this, 'TestBucket', {
+      versioned: true,
+    })
+
+    // new lambda.Function(this, 'NessAPIFunction', {
+    //   runtime: lambda.Runtime.GO_1_X,
+    //   handler: 'index.handler',
+    //   code: lambda.Code.fromAsset(path.join('src', 'ness-api-handler'))
+    // })
   }
 }
