2413268c4ddd0614dd51860769286c392bd39e28
uji <ujiprog@gmail.com>
2020-08-14 00:30:34 +0900

    feat: add lambda function, apigateway resorce

--- lib/ness-api-cdk-stack.ts
+++ lib/ness-api-cdk-stack.ts
@@ -1,20 +1,33 @@
+import * as lambda from '@aws-cdk/aws-lambda';
+import * as apigateway from '@aws-cdk/aws-apigateway';
+import * as s3 from '@aws-cdk/aws-s3';
 import * as cdk from '@aws-cdk/core';
-import * as s3 from '@aws-cdk/aws-s3'
-// import * as lambda from '@aws-cdk/aws-lambda'
-// import * as path from 'path'
 
 export class NessApiCdkStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    new s3.Bucket(this, 'TestBucket', {
-      versioned: true,
+    const bucketName = "testBucket"
+    const bucket = new s3.Bucket(this, bucketName )
+
+    const handler = new lambda.Function(this, 'NessAPIFunction', {
+      runtime: lambda.Runtime.GO_1_X,
+      handler: 'index.handler',
+      code: lambda.Code.fromAsset('./src/ness-function'),
+      environment: {
+        BUCKET: bucketName,
+      }
     })
 
-    // new lambda.Function(this, 'NessAPIFunction', {
-    //   runtime: lambda.Runtime.GO_1_X,
-    //   handler: 'index.handler',
-    //   code: lambda.Code.fromAsset(path.join('src', 'ness-api-handler'))
-    // })
+    bucket.grantReadWrite(handler)
+
+    const api = new apigateway.RestApi(this, "testGateway", {
+      restApiName: "testAPI",
+    })
+    const nessIntegration = new apigateway.LambdaIntegration(handler, {
+      requestTemplates: { "application/json": '{ "statusCode": "200" }' }
+    });
+
+    api.root.addMethod("GET", nessIntegration);
   }
 }
